27c1d2cefb48ee9baa319eb2b431430542eda8bc
Kevin Robayna <me@kevinrobayna.com>
2020-12-01 14:56:11 +0000

    fix solution

--- src/main/kotlin/day1.kt
+++ src/main/kotlin/day1.kt
@@ -4,25 +4,27 @@ import java.util.stream.Collectors
 class Day1(
     private val values: List<Int>
 ) {
-    fun solve(): List<Int> {
-        val solutions: MutableList<Pair<Int, Int>> = mutableListOf()
+    fun solve(): List<Pair<Int, Int>> {
+        val pairs: MutableList<Pair<Int, Int>> = mutableListOf()
 
         for (outLoop in values.indices) {
             for (innerLoop in values.indices) {
-                if (!outLoop.equals(innerLoop) && elementsAdd2020(outLoop, innerLoop)) {
-                    solutions.add(Pair(values[outLoop], values[innerLoop]))
+                if (!outLoop.equals(innerLoop)) {
+                    val first = Math.max(values[outLoop], values[innerLoop])
+                    val second = Math.min(values[outLoop], values[innerLoop])
+                    pairs.add(Pair(first, second))
                 }
             }
         }
 
-        return solutions.stream().map { it.first * it.second }.collect(Collectors.toSet()).toList()
+        return pairs
+            .filter { it.first + it.second == 2020 }
+            .distinct()
     }
-
-    private fun elementsAdd2020(outLoop: Int, innerLoop: Int) = values[outLoop] + values[innerLoop] == 2020
 }
 
 
 fun main() {
     val solutions = Day1(listOf(1721, 979, 366, 299, 675, 1456)).solve()
-    println("solution = $solutions")
+    println("solution = $solutions, x*y = ${solutions[0].first * solutions[0].second}")
 }
--- src/test/kotlin/day1.kt
+++ src/test/kotlin/day1.kt
@@ -5,15 +5,8 @@ class Day1Test {
 
     @Test
     fun example_solution() {
-        val solutions = Day1(listOf(1721, 299)).solve()
-        Assert.assertEquals(1, solutions.size)
-        Assert.assertEquals(514579, solutions[0])
-    }
-
-    @Test
-    fun full_example() {
-        val solutions = Day1(listOf(1721, 979, 366, 299, 675, 1456)).solve()
-        Assert.assertEquals(2, solutions.size)
-        Assert.assertEquals(514579, solutions[0])
+        val solution = Day1(listOf(1721, 979, 366, 299, 675, 1456)).solve()
+        Assert.assertEquals(1, solution.size)
+        Assert.assertEquals(514579, solution[0].first * solution[0].second)
     }
 }
