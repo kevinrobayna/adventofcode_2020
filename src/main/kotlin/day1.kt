7edb9d0f654e2648c59e25f09e9610eaaaddf104
Kevin Robayna <me@kevinrobayna.com>
2020-12-03 11:56:32 +0000

    Day1 part 2

--- src/main/kotlin/day1.kt
+++ src/main/kotlin/day1.kt
@@ -24,6 +24,29 @@ class Day1(
     }
 }
 
+// https://adventofcode.com/2020/day/1
+class Day1Part2(
+    private val values: List<Int>
+) {
+    fun solve(): Triple<Int, Int, Int> {
+        val tuples: MutableList<Triple<Int, Int, Int>> = mutableListOf()
+
+        for (outLoop in values) {
+            for (innerLoop in values) {
+                for (innerInnerLoop in values) {
+                    val points = listOf(outLoop, innerLoop, innerInnerLoop).sorted()
+                    tuples.add(Triple(points[0], points[1], points[2]))
+                }
+            }
+        }
+
+        return tuples
+            .filter { it.first.plus(it.second).plus(it.third) == 2020 }
+            .distinct()
+            .get(0)
+    }
+}
+
 
 fun main() {
     val problem = Day1::class.java.getResource("day1.txt")
@@ -34,4 +57,6 @@ fun main() {
         .collect(Collectors.toList())
     val pair = Day1(problem).solve()
     println("solution = $pair, x*y = ${pair.first * pair.second}")
+    val triple = Day1Part2(problem).solve()
+    println("solution = $triple, x*y*z = ${triple.first * triple.second * triple.third}")
 }
