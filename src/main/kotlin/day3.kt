97ad5d74399141a83ae7fbe2231dde19b44dae3c
Kevin Robayna <me@kevinrobayna.com>
2020-12-03 11:46:46 +0000

    add part2

--- src/main/kotlin/day3.kt
+++ src/main/kotlin/day3.kt
@@ -2,40 +2,69 @@ import java.util.stream.Collectors
 
 // https://adventofcode.com/2020/day/3
 class Day3(
-    private val forest: List<List<String>>
+    private val forest: List<List<String>>,
+    private val down: Int,
+    private val right: Int,
 ) {
     companion object {
         const val TREE = "#"
-        const val DOWN = 1
-        const val RIGHT = 3
     }
 
-    fun solve(): Int {
+    fun solve(): Long {
         var position = Pair(0, 0)
-        var treeCount = 0
+        var treeCount: Long = 0
         for (levels in forest.indices) {
             if (isTree(position.first, position.second)) {
                 treeCount++
             }
             position = next(position.first, position.second)
+            if (position.first > forest.size) {
+                return treeCount
+            }
         }
         return treeCount
     }
 
     fun next(x: Int, y: Int): Pair<Int, Int> {
-        return Pair(x.plus(DOWN), y.plus(RIGHT) % forest[0].size)
+        return Pair(x.plus(down), y.plus(right) % forest[0].size)
     }
 
     fun isTree(x: Int, y: Int): Boolean {
         return forest[x][y] == TREE
     }
+}
 
+class Part2Day3(
+    private val forest: List<List<String>>,
+    private val routes: List<Pair<Int, Int>>,
+) {
+    fun solve(): Long {
+        var solution: Long = 1
+        val solutions = routes.stream().map {
+            val s = Day3(forest, it.first, it.second).solve()
+            println("solution with route $it = $s")
+            s
+        }.collect(Collectors.toList())
+        for (x: Long in solutions) {
+            solution = solution.times(x)
+        }
+
+        return solution
+    }
 }
 
 fun main() {
     val problem: List<List<String>> = buildMapFromString(Day3::class.java.getResource("day3.txt").readText())
-    val solution = Day3(problem).solve()
-    println("solution = $solution")
+    println("solution = ${Day3(problem, 1, 3).solve()}")
+
+    val routes = listOf(
+        Pair(1, 1),
+        Pair(1, 3),
+        Pair(1, 5),
+        Pair(1, 7),
+        Pair(2, 1),
+    )
+    println("solution_Part = ${Part2Day3(problem, routes).solve()}")
 }
 
 fun buildMapFromString(text: String): List<List<String>> = text
--- src/test/kotlin/day3.kt
+++ src/test/kotlin/day3.kt
@@ -4,10 +4,17 @@ import kotlin.test.Test
 class Day3Test {
 
     private val problem = buildMapFromString(Day3Test::class.java.getResource("example_day3.txt").readText())
+    private val routes = listOf(
+        Pair(1, 1),
+        Pair(1, 3),
+        Pair(1, 5),
+        Pair(1, 7),
+        Pair(2, 1),
+    )
 
     @Test
     fun next_from_start_returns_expected_value() {
-        val day3 = Day3(problem)
+        val day3 = Day3(problem, 1, 3)
         val next = day3.next(0, 0)
         Assert.assertEquals(1, next.first)
         Assert.assertEquals(3, next.second)
@@ -16,7 +23,7 @@ class Day3Test {
 
     @Test
     fun second_jump_should_return_tree() {
-        val day3 = Day3(problem)
+        val day3 = Day3(problem, 1, 3)
         val firstJump = day3.next(0, 0)
         val secondJump = day3.next(firstJump.first, firstJump.second)
         Assert.assertEquals(2, secondJump.first)
@@ -26,7 +33,7 @@ class Day3Test {
 
     @Test
     fun third_jump_should_return_open() {
-        val day3 = Day3(problem)
+        val day3 = Day3(problem, 1, 3)
         val nextJump = day3.next(2, 6)
         Assert.assertEquals(3, nextJump.first)
         Assert.assertEquals(9, nextJump.second)
@@ -35,7 +42,7 @@ class Day3Test {
 
     @Test
     fun forth_jump_should_return_tree_as_it_will_start_from_start() {
-        val day3 = Day3(problem)
+        val day3 = Day3(problem, 1, 3)
         val nextJump = day3.next(3, 9)
         Assert.assertEquals(4, nextJump.first)
         Assert.assertEquals(1, nextJump.second)
@@ -44,7 +51,7 @@ class Day3Test {
 
     @Test
     fun fifth_jump_should_return_tree() {
-        val day3 = Day3(problem)
+        val day3 = Day3(problem, 1, 3)
         val nextJump = day3.next(4, 1)
         Assert.assertEquals(5, nextJump.first)
         Assert.assertEquals(4, nextJump.second)
@@ -52,8 +59,39 @@ class Day3Test {
     }
 
     @Test
-    fun example_problem_returns_expected_count_of_trees() {
-        val day3 = Day3(problem)
+    fun type1_problem_returns_expected_count_of_trees() {
+        val day3 = Day3(problem, 1, 1)
+        Assert.assertEquals(2, day3.solve())
+    }
+
+    @Test
+    fun type2_problem_returns_expected_count_of_trees() {
+        val day3 = Day3(problem, 1, 3)
         Assert.assertEquals(7, day3.solve())
     }
+
+    @Test
+    fun type3_problem_returns_expected_count_of_trees() {
+        val day3 = Day3(problem, 1, 5)
+        Assert.assertEquals(3, day3.solve())
+    }
+
+    @Test
+    fun type4_problem_returns_expected_count_of_trees() {
+        val day3 = Day3(problem, 1, 7)
+        Assert.assertEquals(4, day3.solve())
+    }
+
+    @Test
+    fun type5_problem_returns_expected_count_of_trees() {
+        val day3 = Day3(problem, 2, 1)
+        Assert.assertEquals(2, day3.solve())
+    }
+
+    @Test
+    fun part2_problem_returns_expected_count_of_trees_on_paths() {
+        val part2Day3 = Part2Day3(problem, routes)
+        Assert.assertEquals(336, part2Day3.solve())
+    }
+
 }
