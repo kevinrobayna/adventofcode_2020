975adc0b6eef57064ff85328a2fbe7438268b036
Kevin Robayna <me@kevinrobayna.com>
2020-12-03 11:58:14 +0000

    Day1 part 2 (#5)
    * Day1 part 2
    
    * add test

--- src/test/kotlin/day1.kt
+++ src/test/kotlin/day1.kt
@@ -8,4 +8,10 @@ class Day1Test {
         val solution = Day1(listOf(1721, 979, 366, 299, 675, 1456)).solve()
         Assert.assertEquals(514579, solution.first * solution.second)
     }
+
+    @Test
+    fun example_part2_solution() {
+        val solution = Day1Part2(listOf(1721, 979, 366, 299, 675, 1456)).solve()
+        Assert.assertEquals(241861950, solution.first * solution.second * solution.third)
+    }
 }
