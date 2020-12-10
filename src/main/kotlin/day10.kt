8f9b3eb1042640e883f07c9b5f6f8904a0d04a6d
Kevin Robayna <me@kevinrobayna.com>
2020-12-10 15:27:29 +0000

    solve part1

--- src/main/kotlin/day10.kt
+++ src/main/kotlin/day10.kt
@@ -1,18 +1,42 @@
-fun day10ProblemReader(string: String): List<Long> {
+fun day10ProblemReader(string: String): List<Int> {
     return string
         .split("\n")
-        .map { line -> line.toLong() }
+        .map { line -> line.toInt() }
         .toList()
 }
 
 
 // https://adventofcode.com/2020/day/10
 class Day10(
-    private val numbers: List<Long>
+    private val numbers: List<Int>
 ) {
 
-    fun solvePart1(): Long {
-        return 0
+    fun solvePart1(): Int {
+        val goToList = numbers.toMutableList()
+        val n1 = mutableListOf<Int>()
+        val n3 = mutableListOf<Int>()
+        var pointer = 0
+
+        while (goToList.isNotEmpty()) {
+            pointer += when {
+                numbers.contains(pointer.plus(1)) -> {
+                    n1.add(pointer)
+                    goToList.remove(pointer)
+                    1
+                }
+                numbers.contains(pointer.plus(3)) -> {
+                    n3.add(pointer)
+                    goToList.remove(pointer)
+                    3
+                }
+                else -> {
+                    n3.add(pointer)
+                    goToList.remove(pointer)
+                    3
+                }
+            }
+        }
+        return n1.size * n3.size
     }
 
     fun solvePart2(): Long {
