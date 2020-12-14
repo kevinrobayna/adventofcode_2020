afc42c7e10c850f2f562ebbb56f75b6320117e81
Kevin Robayna <me@kevinrobayna.com>
2020-12-14 10:43:17 +0000

    part 1

--- src/main/kotlin/day14.kt
+++ src/main/kotlin/day14.kt
@@ -1,13 +1,16 @@
+import java.math.BigInteger
+
+
 data class Mask(
     val bitmask: String,
-    val memorySettings: List<Pair<Int, Int>>,
+    val memorySettings: List<Pair<String, Long>>,
 )
 
 fun day14ProblemReader(text: String): List<Mask> {
     val maskRegex = "mask\\s=\\s(\\w+)".toRegex()
     val memRegex = "mem\\[(\\d+)\\]\\s=\\s(\\d+)".toRegex()
     var bitmask = ""
-    var memorySettings = mutableListOf<Pair<Int, Int>>()
+    var memorySettings = mutableListOf<Pair<String, Long>>()
     val masks = mutableListOf<Mask>()
     val lines = text.split('\n').withIndex().toList()
     for ((inx, line) in lines) {
@@ -21,7 +24,7 @@ fun day14ProblemReader(text: String): List<Mask> {
             bitmask = mask
         } else {
             val (key, value) = memRegex.matchEntire(line)!!.destructured
-            memorySettings.add(key.toInt() to value.toInt())
+            memorySettings.add(key to value.toLong())
         }
 
         if (inx == lines.size - 1) {
@@ -33,11 +36,23 @@ fun day14ProblemReader(text: String): List<Mask> {
 
 // https://adventofcode.com/2020/day/14
 class Day14(
-    private val problem: List<Mask>,
+    private val masks: List<Mask>,
 ) {
 
-    fun solvePart1(): Int {
-        return 0
+    fun solvePart1(): Long {
+        val memory = mutableMapOf<String, String>()
+        for (program in masks) {
+            program.memorySettings.forEach { (inx, value) ->
+                val valueInBinary = Integer.toBinaryString(value.toInt()).padStart(program.bitmask.length, '0')
+                memory[inx] = program.bitmask.withIndex().map { (inx, char) ->
+                    when (char) {
+                        'X' -> valueInBinary[inx]
+                        else -> char
+                    }
+                }.joinToString("")
+            }
+        }
+        return memory.map { (_, value) -> BigInteger(value, 2).toLong() }.sum()
     }
 
     fun solvePart2(): Long {
