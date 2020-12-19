bac01253a0eca4f7167ba63c58e6a3f2c77c373b
Kevin Robayna <me@kevinrobayna.com>
2020-12-19 22:23:35 +0000

    solve p15

--- src/main/kotlin/day15.kt
+++ src/main/kotlin/day15.kt
@@ -8,7 +8,17 @@ class Day15(
 ) {
 
     fun solvePart1(): Long {
-        return 0
+        val numbers = spokenNumbers.map { it.toLong() }.toMutableList()
+        for (turn in (numbers.size + 1)..2020) {
+            val lastSpoken = numbers[turn - 2]
+            val spokenBefore = numbers.withIndex().filter { (_, value) -> value == lastSpoken }
+            if (spokenBefore.isEmpty() || spokenBefore.size == 1) { // rule 1, first time then add 0
+                numbers.add(0)
+            } else { // rule 2, add difference of indexes
+                numbers.add((turn - 1) - (spokenBefore[spokenBefore.size - 2].index.toLong() + 1))
+            }
+        }
+        return numbers[2020 - 1]
     }
 
     fun solvePart2(): Long {
