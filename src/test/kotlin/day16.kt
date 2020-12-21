6c37b0cd578eb2a8b46a1161e9ac7bc3ed448a6c
Kevin Robayna <me@kevinrobayna.com>
2020-12-21 10:50:12 +0100

    read p16

--- src/test/kotlin/day16.kt
+++ src/test/kotlin/day16.kt
@@ -3,7 +3,19 @@ import org.junit.Test
 
 class Day16Test {
 
-    private val example = Day16Test::class.java.getResource("example_day16.txt").readText()
+    private val example = """
+            class: 1-3 or 5-7
+            row: 6-11 or 33-44
+            seat: 13-40 or 45-50
+
+            7,1,14
+
+            nearby tickets:
+            7,3,47
+            40,4,50
+            55,2,20
+            38,6,12
+        """.trimIndent()
 
     @Test
     fun exampleProblem_is_solvedCorrectly() {
